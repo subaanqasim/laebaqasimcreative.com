7c22b6e0bc9f25ee0e7662bd83b2dc57fc8bbd39
Subaan Qasim <subaan.qasim@gmail.com>
2023-03-13 20:02:47 +0000

    add astro-sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import sanity from "astro-sanity";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.laebaqasimcreative.com",
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    sanity({
+      projectId: "temp",
+      dataset: "production",
+      apiVersion: "2021-03-25",
+      useCdn: true,
+    }),
+  ],
 });
