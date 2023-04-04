be9d1205ffecc8a7e837d90cb6b665d3879cff8a
Subaan Qasim <subaan.qasim@gmail.com>
2023-04-04 14:36:03 +0100

    change from preact to react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
-import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import sanity from "astro-sanity";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.laebaqasimcreative.com",
-  integrations: [
-    tailwind(),
-    sitemap(),
-    sanity({
-      projectId: "temp",
-      dataset: "production",
-      apiVersion: "2021-03-25",
-      useCdn: true,
-    }),
-    preact({ compat: true }),
-  ],
+  integrations: [tailwind(), sitemap(), sanity({
+    projectId: "temp",
+    dataset: "production",
+    apiVersion: "2021-03-25",
+    useCdn: true
+  }), react()]
 });
