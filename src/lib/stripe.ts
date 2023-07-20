068ac707a1f9c941747a17dd3069f3e393b67979
Subaan Qasim <subaan.qasim@gmail.com>
2023-07-20 18:01:09 +0100

    revert to relative path imports

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-import { env } from "@lib/env";
+import { env } from "../../src/lib/env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-11-15",
