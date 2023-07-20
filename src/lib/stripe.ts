1022cb859add76f88daf00de67120758a27dd853
Subaan Qasim <subaan.qasim@gmail.com>
2023-07-20 21:52:08 +0100

    revert to default stripe http client

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,7 +4,7 @@ import { env } from "../../src/lib/env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-11-15",
-  httpClient: Stripe.createFetchHttpClient(),
+  // httpClient: Stripe.createFetchHttpClient(),
 });
 
 export const webCrypto = Stripe.createSubtleCryptoProvider();
