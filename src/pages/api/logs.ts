7a022b1d5afc42a1e1bb16fffe156ba51d835268
Subaan Qasim <subaan.qasim@gmail.com>
2023-07-26 18:17:30 +0100

    test more logs

--- src/pages/api/logs.ts
+++ src/pages/api/logs.ts
@@ -1,4 +1,4 @@
-import { APIRoute } from "astro";
+import type { APIRoute } from "astro";
 
 export const prerender = false;
 
@@ -8,6 +8,12 @@ export const get: APIRoute = ({ request }) => {
   console.log("edge runtime?: ", globalThis.EdgeRuntime);
 
   console.log("url:", request.url);
+
+  console.log("baseurl: ", import.meta.env.BASE_URL);
+  console.log("siteurl: ", import.meta.env.SITE);
+
+  console.log("vercel_url: ", import.meta.env.PUBLIC_VERCEL_URL);
+  console.log("vercel_branch_url: ", import.meta.env.PUBLIC_VERCEL_BRANCH_URL);
 
   return { status: 200, body: "Logs work?" };
 };
