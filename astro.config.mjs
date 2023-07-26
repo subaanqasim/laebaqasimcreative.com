71f8b08cc590a651cf768d031247bf0f910f6afe
Subaan Qasim <subaan.qasim@gmail.com>
2023-07-26 17:01:04 +0100

    change from vercel edge to serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 const siteUrl =
