15be76e346689f519786702d8ac9fcaf6b87eede
Subaan Qasim <subaan.qasim@gmail.com>
2023-07-26 21:42:55 +0100

    remove log testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,16 @@ import vercel from "@astrojs/vercel/edge";
 import { defineConfig } from "astro/config";
 
 const siteUrl =
-  import.meta.env.PUBLIC_VERCEL_ENV === "production"
+  process.env.PUBLIC_VERCEL_ENV === "production"
     ? "https://laebaqasimcreative.com/"
-    : import.meta.env.PUBLIC_VERCEL_ENV === "preview"
-    ? `https://${import.meta.env.PUBLIC_VERCEL_BRANCH_URL}`
+    : process.env.PUBLIC_VERCEL_ENV === "preview"
+    ? `https://${process.env.PUBLIC_VERCEL_BRANCH_URL}`
     : "http://localhost:3000";
 
 console.log("env: ", process.env.PUBLIC_VERCEL_ENV);
-console.log("mode: ", import.meta.env.MODE);
 console.log("branch url: ", process.env.PUBLIC_VERCEL_BRANCH_URL);
 console.log("siteUrl:", siteUrl);
 
-// https://astro.build/config
 export default defineConfig({
   site: siteUrl,
   output: "hybrid",
